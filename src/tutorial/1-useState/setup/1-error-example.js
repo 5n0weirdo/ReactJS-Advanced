3a8af2967149eaea33954cc4315f5b0399bc1fe6
5n0weirdo <snoweirdo@protonmail.com>
2021-10-16 14:00:10 +0200

    Final

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 const ErrorExample = () => {
   let title = "Random Title"
+  const handleClick = () => {
+    title = "Hello People"
+    console.log(title)
+  }
+
   return <React.Fragment>
     <h2>{title}</h2>
+    <button className="btn" onClick={handleClick}>
+      Change Title </button>
   </React.Fragment>
 };
 
