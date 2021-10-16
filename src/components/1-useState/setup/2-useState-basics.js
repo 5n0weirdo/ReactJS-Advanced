797ba93c3bfafe36adbc1ef3f15c861ed8aec6d6
5n0weirdo <snoweirdo@protonmail.com>
2021-10-16 17:13:38 +0200

    useState default value

--- src/components/1-useState/setup/2-useState-basics.js
+++ src/components/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,21 @@
 import React, { useState } from 'react';
 
+//useState > function
+
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  // console.log(useState("hello world "))
+  // const value = useState(7)[0];
+  // const handler = useState(9)[1];
+
+  // console.log(value, handler);
+
+  const [text, setText] = useState("Random Title")
+
+  return (
+    <React.Fragment>
+      <h1>{text}</h1>
+    </React.Fragment>
+  )
 };
 
 export default UseStateBasics;
