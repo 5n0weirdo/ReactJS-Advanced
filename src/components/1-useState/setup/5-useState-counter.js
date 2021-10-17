b4b86fdaf5a9475aca4d6fa6c024cb29e04f9f5b
5n0weirdo <snoweirdo@protonmail.com>
2021-10-17 09:35:03 +0200

    Button functionality added

--- src/components/1-useState/setup/5-useState-counter.js
+++ src/components/1-useState/setup/5-useState-counter.js
@@ -1,16 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateCounter = () => {
-  const [value, setValue] = useState(0)
+  const [value, setValue] = useState(0);
 
+  const resetValue = () => {
+    setValue(0);
+  };
   return (
     <>
       <section style={{ margin: "4rem 0" }}>
         <h2>Regular Counter</h2>
         <h1>{value}</h1>
-        <button className="btn">Decrease</button>
-        <button className="btn">Reset</button>
-        <button className="btn">Increase</button>
+        <button className="btn" onClick={() => setValue(value - 1)}>
+          Decrease
+        </button>
+        <button className="btn" onClick={resetValue}>
+          Reset
+        </button>
+        <button className="btn" onClick={() => setValue(value + 1)}>
+          Increase
+        </button>
       </section>
     </>
   );
