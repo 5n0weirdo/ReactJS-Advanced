d1e2f5a4d8bbfd39e411f63f2afef69f748ae4e6
5n0weirdo <snoweirdo@protonmail.com>
2021-10-17 08:45:51 +0200

    Object values displayed

--- src/components/1-useState/setup/3-useState-array.js
+++ src/components/1-useState/setup/3-useState-array.js
@@ -1,38 +1,34 @@
-import React from 'react';
-import { useState } from 'react/cjs/react.development';
-import { data } from '../../../data';
-
+import React from "react";
+import { useState } from "react/cjs/react.development";
+import { data } from "../../../data";
 
 const UseStateArray = () => {
-  const [people, setPeople] = useState(data)
+  const [people, setPeople] = useState(data);
 
   const clickHandler = () => {
     setPeople([]);
-  }
+  };
   const removeItem = (id) => {
     const newPeople = people.filter((person) => person.id !== id);
     setPeople(newPeople);
-  }
+  };
 
   return (
     <>
-      {
-        people.map((person) => {
-          const { id, name } = person
-          return (<div key={id} className="item">
+      {people.map((person) => {
+        const { id, name } = person;
+        return (
+          <div key={id} className="item">
             <h4>{name}</h4>
             <button onClick={() => removeItem(id)}>Remove</button>
           </div>
-          )
-        })
-      }
+        );
+      })}
       <button className="btn" onClick={clickHandler}>
         Clear Items
       </button>
     </>
-  )
-
-
+  );
 };
 
 export default UseStateArray;
---
+++ src/components/1-useState/setup/4-useState-object.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+const UseStateObject = () => {
+  const [person, setPerson] = useState({
+    name: "Persy",
+    age: 18,
+    message: "Random message",
+  });
+
+  return (
+    <>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>{person.message}</h3>
+    </>
+  );
+};
+
+export default UseStateObject;
