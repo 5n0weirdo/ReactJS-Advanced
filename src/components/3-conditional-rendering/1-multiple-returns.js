dfd9655fdea761a3bef7416b2f9e794bf7bc2431
5n0weirdo <snoweirdo@protonmail.com>
2021-10-25 12:06:47 +0200

    Added useStates

--- src/components/3-conditional-rendering/1-multiple-returns.js
+++ src/components/3-conditional-rendering/1-multiple-returns.js
@@ -1,13 +1,32 @@
 import React, { useState, useEffect } from "react";
 const url = "https://api.github.com/users/QuincyLarson";
 const MultipleReturns = () => {
-  const [loading, setLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
 
-  if (loading) {
-    return <h2>Loading...</h2>;
+  const [isError, setIsError] = useState(false);
+
+  const [user, setUser] = useState("Default user");
+
+  if (isLoading) {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    );
   }
-  return "Hello World";
-  return <h2>multiple returns</h2>;
+
+  if (isError) {
+    return (
+      <div>
+        <h1>Error...</h1>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <h1>{user}</h1>
+    </div>
+  );
 };
 
 export default MultipleReturns;
