f55cb7699d2d280c307b12b6ac10de251a82a501
5n0weirdo <snoweirdo@protonmail.com>
2021-10-25 04:36:17 +0200

    Button functionality added

--- src/components/2-useEffect/1-useEffect-basics.js
+++ src/components/2-useEffect/1-useEffect-basics.js
@@ -3,10 +3,17 @@ import React, { useState, useEffect } from "react";
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
+  const [value, setValue] = useState(0);
+  const countHandler = () => {
+    setValue(value + 1);
+  };
   useEffect(() => {});
   return (
     <div>
-      <button className="btn">Click</button>
+      <h1>{value}</h1>
+      <button className="btn" onClick={countHandler}>
+        Click Me
+      </button>
     </div>
   );
 };
