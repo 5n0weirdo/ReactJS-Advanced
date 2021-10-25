e16b569d740f4746d79ec503e5197241b544cf3e
5n0weirdo <snoweirdo@protonmail.com>
2021-10-25 04:32:30 +0200

    Added a button

--- src/components/2-useEffect/1-useEffect-basics.js
+++ src/components/2-useEffect/1-useEffect-basics.js
@@ -1,9 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 // by default runs after every re-render
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  useEffect(() => {});
+  return (
+    <div>
+      <button className="btn">Click</button>
+    </div>
+  );
 };
 
 export default UseEffectBasics;
