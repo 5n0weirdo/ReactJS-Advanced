81cbdcdc2166416c8bd4a607713f5f4d8742c7df
5n0weirdo <snoweirdo@protonmail.com>
2021-10-25 11:11:29 +0200

    Change size of screen

--- src/components/2-useEffect/2-useEffect-cleanup.js
+++ src/components/2-useEffect/2-useEffect-cleanup.js
@@ -5,6 +5,14 @@ import React, { useState, useEffect } from "react";
 
 const UseEffectCleanup = () => {
   const [size, setSize] = useState(window.innerWidth);
+
+  const checkSize = () => {
+    setSize(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", checkSize);
+  });
   return (
     <div>
       <h1>Window</h1>
