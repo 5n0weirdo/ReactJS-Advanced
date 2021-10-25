5e023c7023bf445a31b36011ff4aaad0062fda4a
5n0weirdo <snoweirdo@protonmail.com>
2021-10-25 12:53:38 +0200

    Button functionailty added

--- src/components/3-conditional-rendering/2-short-circuit.js
+++ src/components/3-conditional-rendering/2-short-circuit.js
@@ -4,13 +4,17 @@ import React, { useState } from "react";
 
 const ShortCircuit = () => {
   const [text, setText] = useState("");
-  const firstValue = text || "hello world";
-  const secondValue = text && "hello world";
+
+  // const firstValue = text || "hello world";
+  // const secondValue = text && "hello world";
 
   return (
     <div>
-      <h1>{firstValue}</h1>
-      <h1>Value: {secondValue}</h1>
+      {/* <h1>{firstValue}</h1>
+      <h1>Value: {secondValue}</h1> */}
+      <h1>{text || "Ersy"}</h1>
+      <button className="btn">Toggle error</button>
+      {text && <h1>Hello.</h1>}
     </div>
   );
 };
