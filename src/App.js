88f2c8c8fc3694ebe670e30316e9aed59f6da224
5n0weirdo <snoweirdo@protonmail.com>
2021-10-25 11:53:09 +0200

    Setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Setup from "./components/2-useEffect/3-useEffect-fetch-data";
+import Setup from "./components/3-conditional-rendering/1-multiple-returns";
 
 function App() {
   return (
