a03199956c20114448716fb76efc8757690f6f4c
5n0weirdo <snoweirdo@protonmail.com>
2021-11-06 19:33:04 +0200

    Added conditions

--- src/components/4-forms/1-controlled-inputs.js
+++ src/components/4-forms/1-controlled-inputs.js
@@ -3,10 +3,17 @@ import React, { useState } from "react";
 const ControlledInputs = () => {
   const [firstName, setFirstName] = useState("");
   const [email, setEmail] = useState("");
+  const [people, setPeople] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(firstName, email);
+
+    if (firstName && email) {
+      const person = { firstName, email };
+      console.log(person);
+    } else {
+      console.log("empty");
+    }
   };
 
   return (
